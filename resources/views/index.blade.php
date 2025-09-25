7393cf08916101a8644d6483528f09836000bd06
Michal-Dlu <turyferarz@tutanota.com>
2025-09-25 13:45:08 +0200

    Utworzenie wstępnego layoutu i controllera ExpenseController

---
+++ resources/views/index.blade.php
@@ -0,0 +1,64 @@
+
+      @extends('layouts.appIndex')
+      @section('content')
+        <!-- Masthead-->
+      <header class="masthead bg-primary text-white text-center" style="padding:10px">
+                <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-0" src="assets/img/avataaars.svg" alt="..." />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0" href="{{route ('index')}}" style="font-size:30px; padding:3px">Moje wydatki</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line" style="font-size:20px; padding:3px"></div>
+                    <div class="divider-custom-icon" style="font-size:20px; padding:3px"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line" style="font-size:20px; padding:3px"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0" style="font-size:20px; padding:3px">Pomagam kontrolować Twoje wydatki</p>
+            </div>
+        </header>
+        <!-- Portfolio Section-->
+        <section class="page-section portfolio" id="portfolio" style="padding:20px">
+            <div class="container">
+                <!-- Portfolio Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0" style="font-size:30px">Menu</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Portfolio Grid Items-->
+                <div class="row justify-content-center">
+                    <!-- Portfolio Item 1-->
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto"><a class="navbar-brand" href="{{ route('shops.shopList') }}">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <div class="bg-dark bg-opacity-50 text-white p-3">
+                            Sklepy
+                            </div>
+                            <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." /></a>
+                        </div>
+                    </div>
+                    <!-- Portfolio Item 2-->
+                    <div class="col-md-6 col-lg-4 mb-5">
+                        <div class="portfolio-item mx-auto"><a class="navbar-brand" href="{{ route('expenses.expensesList') }}">
+                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <div class="bg-dark bg-opacity-50 text-white p-3">
+                            Wydatki
+                            </div>
+                            <img class="img-fluid" src="assets/img/portfolio/cake.png" alt="..." /></a>
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+        </section>
+
+         
+      @endsection
+   
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpensesController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+Route::get('/shops/shopList', function () {
+    return view('shops.shopList');
+})->name('shops.shopList');
+Route::get('/shops/form', function () {
+    return view('shops.form');
+})->name('shops.form');
+Route::get('/expenses/expensesList', [ExpensesController::class, 'list'])->name('expenses.expensesList');
+Route::get('/expenses/form', [ExpensesController::class, 'create'])->name('expenses.form');
